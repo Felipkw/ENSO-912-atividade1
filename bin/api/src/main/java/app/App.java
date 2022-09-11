29c6eb6baa756e3b50913fcb5bbc8c4b88be5296
Felipkw <bfelipedelima@gmail.com>
2022-09-11 23:42:53 +0000

    Criação da função divisão

--- bin/api/src/main/java/app/App.java
+++ bin/api/src/main/java/app/App.java
@@ -7,10 +7,11 @@ public class App extends Jooby {
   {
 
     mvc(new Index());
+    mvc(new FuncaoDivisao());
   }
 
   public static void main(final String[] args) {
     runApp(args, App::new);
+  
   }
-
 }
--- bin/api/src/main/java/app/Index.java
+++ bin/api/src/main/java/app/Index.java
@@ -4,14 +4,23 @@ import io.jooby.annotations.*;
 
 
 @Path("/")
+
 public class Index {
   
-  // Apresentação: 
+  // Junção dos elementos 
   @GET
-  public String sayHi() {
-    return "Bem-vindo a API calculadora CEEF";
+  public String All() {  
+    String all = sayHi() + showDivisao();
+    return all;
   }
 
+  // Apresentação da calculadora
+  public String sayHi() {
+    return "Bem-vindo a API calculadora CEEF!";
+  }
 
-
+  // Ensinando a usar a função 'divisao' da calculadora
+  public String showDivisao(){
+    return "\n\nPara utilizar a funcao 'divisao' digite a frente da URL: divisao/(dividendo)/(divisor)";
+  }
 }
