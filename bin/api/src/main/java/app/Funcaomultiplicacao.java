4aed3f79d4951e2c2528eebfcae4481a035051af
Emanuely Tauany <etsf1@aluno.ifal.edu.br>
2022-09-12 18:42:00 +0000

    Criação da função multiplicação

---
+++ bin/api/src/main/java/app/Funcaomultiplicacao.java
@@ -0,0 +1,28 @@
+package app;
+ 
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+ 
+ 
+@Path("/multiplicacao/{mult1}/{multi2}")
+public class Funcaomultiplicacao {
+ 
+  // Função divisão:
+  @GET
+  public double multiplicacao(@PathParam("mult1") String opStr, @PathParam("mult2") String opStr2){
+ 
+    try{
+        double op = Double.parseDouble(opStr);
+        double op2 = Double.parseDouble(opStr2);
+ 
+        // Resposta caso ocorra uma divisão por zero:
+       
+ 
+        double mult = op*op2;
+        return mult;    
+    } catch (NumberFormatException nfe) {
+        throw new BadRequestException("Operador inválido. Verifique se você digitou corretamente");
+    }
+ 
+  }
+}
