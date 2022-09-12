20dd13ac11caaae97286b642ff4265942d32984d
clarajrs <cjrs2@aluno.ifal.edu.br>
2022-09-12 17:17:26 +0000

    Código corrijido

--- bin/api/src/main/java/app/Soma.java
+++ bin/api/src/main/java/app/Soma.java
@@ -11,8 +11,10 @@ public class Soma {
         try{
             int num1 = Integer.parseInt(num1Str);
             int num2 = Integer.parseInt(num2Str);
+ 
+            int n3 = num1 + num2;
+            return n3;
 
-            return num1 + num2;
         }catch(NumberFormatException nfe){
             throw new BadRequestException("Número inválido");
 
