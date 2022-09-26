bab065f3ebf47cd7fa5097d9d88da3ef289a564f
flaviaccordeiro <fecc1@aluno.ifal.edu.br>
2022-09-26 18:15:33 +0000

    Crianção de teste de unidade: Subtração

---
+++ bin/api/src/test/java/app/subtracaoTest.java
@@ -0,0 +1,34 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.beans.Transient;
+
+public class subtracaoTest {
+
+    @Test
+        public void makeSubTest(){
+
+            FuncaoSubtracao sub = new FuncaoSubtracao();
+            double resultado = sub.calcular("13", "3");
+            assertEquals (10.0, resultado);
+        }
+
+        @Test
+        public void makeSubTest2(){
+
+            FuncaoSubtracao sub = new FuncaoSubtracao();
+            double resultado = sub.calcular("-1", "1");
+            assertEquals (-2.0, resultado);
+        }
+
+        @Test
+        public void makeSubTest3(){
+
+            FuncaoSubtracao sub = new FuncaoSubtracao();
+            double resultado = sub.calcular("8.4", "6.2");
+            assertEquals (2.2, resultado);
+        }
+}
