e5d725b2797f24fecdfe5ece2b8d36effc5ccd0a
Emanuely Tauany <etsf1@aluno.ifal.edu.br>
2022-09-26 17:58:54 +0000

    Criação de testes de unidade: Multiplicação

---
+++ bin/api/src/test/java/app/MultiplicacaoTest.java
@@ -0,0 +1,43 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.exception.BadRequestException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class MultiplicacaoTest{
+
+FuncaoMultiplicacao multiplicacao;
+@Test
+public void Multpor0(){
+    multiplicacao = new FuncaoMultiplicacao();
+    double resultado=  multiplicacao.multiplicacao("0", "5");
+    assertEquals(0.0, resultado);
+
+}
+@Test
+public void Multiplica1x5(){
+    multiplicacao= new FuncaoMultiplicacao();
+    double resultado= multiplicacao.multiplicacao("1","5");
+    assertEquals(5, resultado);
+
+}
+@Test
+public void Multiplica1negativo(){
+    multiplicacao=new FuncaoMultiplicacao();
+    double resultado= multiplicacao.multiplicacao("1","-1");
+    assertEquals(-1, resultado);
+
+}
+
+}
+
+
+
+
+
+
